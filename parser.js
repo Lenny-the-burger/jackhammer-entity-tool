8b614a85fc8119e8887a2eb48c3d5a5ce6548383
Lenny the burger <tednboss@gmail.com>
2022-06-24 13:59:55 -0400

    files for parser creation + .gitignore

---
+++ parser.js
@@ -0,0 +1,4 @@
+// this script parses the fgd files and builds a useable json file
+// why use js? i wouldve used python but I found a package for parsing fgd files
+// and it was written in js so might as well use it
+
